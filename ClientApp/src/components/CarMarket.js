82d406dd323aeff4253e798dd92250ff6b2ce0e3
Mihai-Stefan <mihaiste@gmail.com>
2021-06-17 21:57:50 +0300

    added from Victor

--- ClientApp/src/components/CarMarket.js
+++ ClientApp/src/components/CarMarket.js
@@ -9,147 +9,179 @@ import Card from "react-bootstrap/Card";
 import "../App.css";
 
 const CarMarket = () => {
-    let showAforted = true;
-    const [reversed, setReversed] = useState();
-    const [userPoints, setUserPoints] = useState(100);
-    const [carItems, setCarItems] = useState();
-    const [tempCarItems, setTempCarItems] = useState();
-    const [companyList, setCompanyList] = useState([]);
-    const [selectCompany, setSelectCompany] = useState([]);
+  
+  const [reversed ,setReversed] = useState();
+  const [userPoints, setUserPoints] = useState(0);
+  const [carItems, setCarItems] = useState();
+  const [tempCarItems, setTempCarItems] = useState();
+  const [companyList, setCompanyList] = useState([]);
+  const [selectCompany, setSelectCompany] = useState([]);
     const [sortValue, setSortValue] = useState("");
-    const [isUp, setIsUp] = useState("false");
+  const [isUp, setIsUp] = useState("false");
+  const [userID,setUserID] = useState(localStorage.getItem('userID'));
+  const [usersData,setUsersData] = useState();
+  const [user,setUser] = useState();
+  
+  useEffect(() => {
+    axios
+      .get("https://localhost:44316/api/CarItems")
+      .then((response) => {
+        console.log("=============response.data.Resultsce");
+        setCarItems(response.data);
+        setTempCarItems(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios
-            .get("https://localhost:44316/api/CarItems")
-            .then((response) => {
-                console.log("=============response.data.Resultsce");
-                setCarItems(response.data);
-                setTempCarItems(response.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
 
-    const [string, setString] = React.useState("");
-    function handleChange(newValue) {
-        setString(newValue);
-    }
-    const handleToggle = () => {
-        setIsUp(!isUp);
-    };
-    function handleCheckChange(event) {
-        var company = event.target.value;
-
-        if (selectCompany.includes(company)) {
-            const newList = selectCompany.filter((com) => com !== company);
-            setSelectCompany(newList);
-        } else {
-            setSelectCompany([...selectCompany, company]);
-            console.log(selectCompany);
+  useEffect(() => {
+    axios
+      .get("https://localhost:44316/api/Employees")
+      .then((response) => {
+        console.log("=============response.data.EMPLOYEE");
+        console.log(userID);
+        console.log(response.data);
+        for(var i=0;i<response.data.length;i++){
+          console.log(response.data[i]);
+          console.log(userID);
+          if(response.data[i].registrationId === userID)
+          {setUsersData(response.data[i]);
+          setUserPoints(response.data[i].rankPoints);}
         }
-        filter(selectCompany);
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [userID]);
+  
+
+  console.log(usersData);
+
+  
+
+  
+  const [string, setString] = React.useState("");
+  function handleChange(newValue) {
+    setString(newValue);
+
+  }
+  const handleToggle = () => {
+    setIsUp(!isUp);
+  };
+  function handleCheckChange(event) {
+    var company = event.target.value;
+
+    if (selectCompany.includes(company)) {
+      const newList = selectCompany.filter((com) => com !== company);
+      setSelectCompany(newList);
+    } else {
+      setSelectCompany([...selectCompany, company]);
+      console.log(selectCompany);
     }
+    filter(selectCompany);
+  }
 
-
+  
     function handleSelectChange(event) {
-        var str = event.target.value;
-        var number = str.slice(-1);
+    var str = event.target.value;
+    var number = str.slice(-1);
         if (number === "1") {
-            setReversed(false);
-        }
-        else if (number === "2") {
-            setReversed(true);
-        }
-        console.log(reversed);
-        setSortValue(str.substring(0, str.length - 1));
+      setReversed(false);
     }
+        else if (number === "2") {
+      setReversed(true);
+    }
+    console.log(reversed);
+    setSortValue(str.substring(0, str.length - 1));
+  }
 
-    const filter = (selectComp) => {
-        let temp = carItems;
-
-        if (selectCompany.length > 0) {
-            temp = temp.filter((item) =>
-                selectCompany.includes(item.company)
-            );
-            setTempCarItems(temp);
-        }
-
+  const filter = (selectComp) => {
+    let temp = carItems;
+    
+    if (selectCompany.length > 0) {
+      temp = temp.filter((item) =>
+        selectCompany.includes(item.company)
+      );
+      setTempCarItems(temp);
+    }
+  
         else {
-            setTempCarItems(carItems);
-        }
-    };
+      setTempCarItems(carItems);
+    }
+  };
 
-    useEffect(filter, [selectCompany]);
+  useEffect(filter, [selectCompany]);
 
 
-    return (
-        <div>
-            <h1>Cars</h1>
-            <div className="carMarket-upper-body">
-                <SearchBar value={string} onChange={handleChange} />
-                <h3 className="car-market-title">You have: {userPoints} points</h3>
-                <h3 className="car-market-title">Theese are the cars you can afford</h3>
-            </div>
-            <Form className="select-form">
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Sort By</Form.Label>
-                    <Form.Control as="select" onChange={handleSelectChange}>
-                        <option> </option>
-                        <option value="valuePoints1">Points Ascending</option>
-                        <option value="valuePoints2">Points Descending</option>
-                        <option value="topSpeedKmph2">Top Speed</option>
-                        <option value="maxPowerBhp2">Horse Power</option>
-
-                    </Form.Control>
-                </Form.Group>
-            </Form>
-
-            <div className="container-cars">
-                <h1 className="hidden">
-                    {carItems &&
-                        carItems.map((element) =>
-                            companyList.includes(element.company)
-                                ? null
-                                : companyList.push(element.company)
-                        )}
-                </h1>
-                < div className="filter">
-                    <Accordion defaultActiveKey="0" className="filter">
-                        <Card >
-                            <Accordion.Toggle as={Card.Header} eventKey="0" onClick={handleToggle}>
-                                <p><span className="select-company">Select Company</span><i className={isUp ? "arrow up" : "arrow down"}></i></p>
-                            </Accordion.Toggle>
-                            <Accordion.Collapse eventKey="0">
-                                <Form.Group controlId="formBasicCheckbox">
-                                    {companyList &&
-                                        companyList.map((company) => (
-                                            <Form.Check
-                                                type="checkbox"
-                                                label={company}
-                                                id={company}
-                                                value={company}
-                                                onChange={handleCheckChange}
-                                            />
-                                        ))}
-                                </Form.Group>
-                            </Accordion.Collapse>
-                        </Card>
-                    </Accordion>
-                </div>
-                <CardContainer
-                    string={string}
-                    data={tempCarItems}
-                    userPoints={userPoints}
-                    showAforted={showAforted}
-                    sortValue={sortValue}
-                    reversed={reversed}
-                />
-
-            </div>
+  return (
+    <div>
+      <h1>Cars</h1>
+      <div className="carMarket-upper-body">
+        <SearchBar value={string} onChange={handleChange} />
+        <h3 className="car-market-title">You have: {userPoints} points</h3>
+        <h3 className="car-market-title">Theese are the cars available on the market: </h3>
         </div>
-    );
+        <Form className="select-form">
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Sort By</Form.Label>
+            <Form.Control as="select" onChange={handleSelectChange}>
+              <option> </option>
+              <option value="valuePoints1">Points Ascending</option>
+              <option value="valuePoints2">Points Descending</option>
+              <option value="topSpeedKmph2">Top Speed</option>
+              <option value="maxPowerBhp2">Horse Power</option>
+              
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      
+      <div className="container-cars">
+        <h1 className="hidden">
+          {carItems &&
+            carItems.map((element) =>
+              companyList.includes(element.company)
+                ? null
+                : companyList.push(element.company)
+            )}
+        </h1>
+        < div className="filter">
+        <Accordion defaultActiveKey="0" className="filter">
+          <Card >
+            <Accordion.Toggle as={Card.Header} eventKey="0" onClick={handleToggle}>
+              <p><span className="select-company">Select Company</span><i className={isUp ? "arrow up" : "arrow down"}></i></p>
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Form.Group controlId="formBasicCheckbox">
+                {companyList &&
+                  companyList.map((company) => (
+                    <Form.Check
+                      type="checkbox"
+                      label={company}
+                      id={company}
+                      value={company}
+                      onChange={handleCheckChange}
+                    />
+                  ))}
+              </Form.Group>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+        </div>
+        <CardContainer
+          string={string}
+          data={tempCarItems}
+          userPoints={userPoints}
+          
+          sortValue={sortValue}
+          reversed={reversed}
+        />
+        
+      </div>
+    </div>
+  );
 };
 
 export default CarMarket;
--- ClientApp/src/components/CardContainer.jsx
+++ ClientApp/src/components/CardContainer.jsx
@@ -4,64 +4,63 @@ import "../App.css";
 import CarMarket from "./CarMarket";
 
 
-function CardContainer({ string, data, userPoints, showAforted, sortValue, reversed }) {
+function CardContainer({ string, data, userPoints, sortValue, reversed }) {
 
-    const [selectedMakers, setSelectedMakers] = useState();
+  const [selectedMakers,setSelectedMakers] = useState();
+  
+  let carItems= data;
+  
+  if(sortValue&&carItems)
+  {console.log(reversed);
+    
+    carItems.sort(GetSortOrder(sortValue,reversed));}
+  
+  console.log(selectedMakers)
+  function GetSortOrder(prop,reversed) {    
+    return function(a, b) {    
+        if (a[prop] > b[prop]) {
+          if(reversed===true)    
+            {return -1; } 
+          else{return 1;}    
+        } else if (a[prop] < b[prop]) {    
+          if(reversed===true)    
+          {return 1; } 
+          else{return -1;}     
+        }    
+        return 0;    
+    }    
+}  
 
-    let carItems = data;
-    if (showAforted && carItems) {
-        carItems = carItems.filter((item) => item.valuePoints < userPoints)
-    }
-    if (sortValue && carItems) {
-        console.log(reversed);
-
-        carItems.sort(GetSortOrder(sortValue, reversed));
-    }
-
-    console.log(selectedMakers)
-    function GetSortOrder(prop, reversed) {
-        return function (a, b) {
-            if (a[prop] > b[prop]) {
-                if (reversed === true) { return -1; }
-                else { return 1; }
-            } else if (a[prop] < b[prop]) {
-                if (reversed === true) { return 1; }
-                else { return -1; }
-            }
-            return 0;
-        }
-    }
-
-    return (
-        <div className="card-container">
-            {carItems && carItems.map((object, index) =>
-
-                string != "" ?
-                    (object.company.includes(string.toLowerCase())
-                        || object.fuel.includes(string.toLowerCase())
-                        || object.bodyType.includes(string.toLowerCase())
-                        || object.generation.includes(string.toUpperCase())
-                        || object.model.includes(string.toLowerCase())
-
-                    )
-
-                        ?
-                        (
-                            <div className="car-card" key={index}>
-                                <CardContent key={index} id={index} element={object} />
-                            </div>
-                        )
-                        :
-                        null
-                    : (
-                        <div className="car-card" key={index}>
-                            <CardContent key={index} id={index} element={object} />
-                        </div>
-                    )
-
-            )}
-        </div>
-    );
+  return (
+    <div className="card-container">
+      {carItems  && carItems.map((object , index ) => 
+      
+      string != "" ?
+      (object.company.includes(string.toLowerCase())
+      || object.fuel.includes(string.toLowerCase())
+      || object.bodyType.includes(string.toLowerCase())
+      || object.generation.includes(string.toUpperCase())
+      || object.model.includes(string.toLowerCase())
+      
+      )
+      
+      ?
+      ( 
+        <div className="car-card" key={index}>
+        <CardContent key={index} id={index} element={object} afforted={object.valuePoints<userPoints}/>
+        </div>  
+      )
+      :
+      null
+      :(
+      <div className="car-card" key={index}> 
+      <CardContent key={index} id={index} element={object} afforted={object.valuePoints<userPoints}/>
+      </div>   
+      )
+      
+      )}
+    </div>
+  );
 }
 
 export default CardContainer;
